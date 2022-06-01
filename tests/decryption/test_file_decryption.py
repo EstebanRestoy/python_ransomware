6e7445d3ce4648022e18091b868543344837a85d
Esteban Restoy <esteban.restoy@etu.uca.fr>
2022-06-01 14:19:44 +0200

    [TESTING] some change to pass the ci

--- tests/decryption/test_file_decryption.py
+++ tests/decryption/test_file_decryption.py
@@ -10,14 +10,14 @@ from modules.cryptography.file_encryption import encrypt_file_name, encrypt_file
 
 def test_decrypt_file_name(create_file, get_fernet):
     """
-    This function test if a file name is successfully encrypted
+    This function test if a file name is successfully decrypted
     :param create_file: the file create by tmp_path
     :param get_fernet: the fernet instance with key loaded
     """
     path_to_file, file_name = split(create_file)
     encrypted_name = encrypt_file_name(create_file, get_fernet)
-    decrypt_file_name(path_to_file + '\\' + encrypted_name, get_fernet)
-    assert exists(path_to_file + '\\' + file_name)
+    decrypt_file_name(path_to_file + '/' + encrypted_name, get_fernet)
+    assert exists(path_to_file + '/' + file_name)
 
 
 def test_decrypt_file_content(create_file, get_fernet):
